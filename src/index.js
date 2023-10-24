501b052352bd3789f7c80620642c95108b7a068a
azat <eric@example.com>
2023-10-24 11:26:29 +0500

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let sArray = [];
+    let sp = str.split("");
+    let br = [];
+    for(let i = 0; i < bracketsConfig.length; i += 1){
+        br.push(bracketsConfig[i].join(""))
+    }
+
+    for(let i = 0; i < sp.length; i += 1) {
+
+        if(br.includes(sArray[sArray.length-1] + sp[i])) {
+            sArray.pop()
+        } else {
+            sArray.push(sp[i]);
+        }
+    }
+    return !sArray.length
 }
